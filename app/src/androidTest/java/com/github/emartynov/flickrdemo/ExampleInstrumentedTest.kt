c456d99e50bf3a5a59a6bfd90f138ca141515ecd
Eugen Martynov <eugenemartynov@gmail.com>
2019-06-11 23:13:06 +0200

    Fix dummy espresso test

--- app/src/androidTest/java/com/github/emartynov/flickrdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/emartynov/flickrdemo/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package com.github.emartynov.flickrdemo
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +17,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.github.emartynov.flickrdemo", appContext.packageName)
     }
 }
