97a86986dbbdb44e7704157c62f981645ca363e8
Eugen Martynov <eugenemartynov@gmail.com>
2019-06-14 01:23:05 +0200

    Update stubs to support required functionality in the tests

--- app/src/test/java/com/github/emartynov/flickrdemo/common/async/AsyncStub.kt
+++ app/src/test/java/com/github/emartynov/flickrdemo/common/async/AsyncStub.kt
@@ -3,7 +3,20 @@ package com.github.emartynov.flickrdemo.common.async
 import java.util.concurrent.Callable
 
 class AsyncStub : Async {
-    override fun <T> queue(job: Callable<T>, callback: (T) -> Unit) {
-        callback(job.call())
+    var isCancelled = false
+        private set
+
+    var execute = true
+
+    override fun cancelAll() {
+        isCancelled = true
+    }
+
+    override fun cancel(tag: Any) {
+        isCancelled = true
+    }
+
+    override fun <T> queue(job: Callable<T>, callback: (T) -> Unit, tag: Any) {
+        if (execute) callback(job.call())
     }
 }
--- app/src/test/java/com/github/emartynov/flickrdemo/common/async/ExecutorStub.kt
+++ app/src/test/java/com/github/emartynov/flickrdemo/common/async/ExecutorStub.kt
@@ -1,17 +1,39 @@
 package com.github.emartynov.flickrdemo.common.async
 
-import java.util.concurrent.Executor
+import java.util.concurrent.*
 
-class ExecutorStub : Executor {
-    private var scheduledJobs = mutableListOf<Runnable>()
+class ExecutorStub : AbstractExecutorService() {
+    private val scheduledJobs = mutableListOf<Runnable>()
+    private val futureTasks = mutableListOf<FutureTask<Unit>>()
+
+    override fun isTerminated() = false
 
     override fun execute(command: Runnable) {
+        throw IllegalArgumentException("We don't execute since we want to cancel it in future")
+    }
+
+    override fun shutdown() {}
+
+    override fun shutdownNow() = scheduledJobs
+
+    override fun isShutdown() = false
+
+    override fun awaitTermination(timeout: Long, unit: TimeUnit) = false
+
+    override fun submit(command: Runnable): Future<*> {
         scheduledJobs.add(command)
+
+        val futureTask = FutureTask(Callable { })
+        futureTasks.add(futureTask)
+
+        return futureTask
     }
 
     fun runJob() {
         scheduledJobs.forEach { it.run() }
-
         scheduledJobs.clear()
+        futureTasks.clear()
     }
+
+    fun areAllCancelled() = futureTasks.all { it.isCancelled }
 }
