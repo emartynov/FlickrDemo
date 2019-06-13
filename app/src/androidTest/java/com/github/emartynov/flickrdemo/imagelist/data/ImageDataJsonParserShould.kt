e67068d4bd2b8e90ed84ea6c788b17864b5edc04
Eugen Martynov <eugenemartynov@gmail.com>
2019-06-13 22:09:24 +0200

    Update ImageDataJsonParser to support new method

--- app/src/androidTest/java/com/github/emartynov/flickrdemo/imagelist/data/ImageDataJsonParserShould.kt
+++ app/src/androidTest/java/com/github/emartynov/flickrdemo/imagelist/data/ImageDataJsonParserShould.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ImageDataParserShould {
+class ImageDataJsonParserShould {
     private val parser = ImageDataJsonParser()
 
     @Test
--- app/src/main/java/com/github/emartynov/flickrdemo/imagelist/data/ImageDataJsonParser.kt
+++ app/src/main/java/com/github/emartynov/flickrdemo/imagelist/data/ImageDataJsonParser.kt
@@ -4,15 +4,14 @@ import com.github.emartynov.flickrdemo.common.json.JsonParser
 import org.json.JSONObject
 
 class ImageDataJsonParser : JsonParser<ImageData> {
-    override fun parse(json: String): ImageData {
-        val jsonObject = JSONObject(json)
-
-        return ImageData(
+    override fun parse(jsonObject: JSONObject) =
+        ImageData(
             id = jsonObject.getString("id"),
             secret = jsonObject.getString("secret"),
             server = jsonObject.getString("server"),
             farm = jsonObject.getInt("farm"),
             title = jsonObject.getString("title")
         )
-    }
+
+    override fun parse(json: String) = parse(JSONObject(json))
 }
