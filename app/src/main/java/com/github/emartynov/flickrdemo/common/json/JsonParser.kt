af6011c12945c455bbff48ca8ae6cd3f0345a87c
Eugen Martynov <eugenemartynov@gmail.com>
2019-06-13 22:08:53 +0200

    Add parse method from JsonObject

--- app/src/main/java/com/github/emartynov/flickrdemo/common/json/JsonParser.kt
+++ app/src/main/java/com/github/emartynov/flickrdemo/common/json/JsonParser.kt
@@ -1,5 +1,8 @@
 package com.github.emartynov.flickrdemo.common.json
 
+import org.json.JSONObject
+
 interface JsonParser<T> {
     fun parse(json: String): T
+    fun parse(jsonObject: JSONObject): T
 }
