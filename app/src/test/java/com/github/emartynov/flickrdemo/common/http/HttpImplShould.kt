53fa7a097e2b9e48c3a17ad3a18bf35e9fa29527
Eugen Martynov <eugenemartynov@gmail.com>
2019-06-13 22:49:58 +0200

    Add comment to the integration test

--- app/src/test/java/com/github/emartynov/flickrdemo/common/http/HttpImplShould.kt
+++ app/src/test/java/com/github/emartynov/flickrdemo/common/http/HttpImplShould.kt
@@ -6,6 +6,7 @@ import org.junit.Test
 class HttpImplShould {
     private val http = HttpImpl()
 
+    // This is an integration test - it will fail without internet access
     @Test
     fun `Fetch binary http content`() {
         val content = http.get("https://api.github.com/gists/9d2bc8dd7def1fe17702ba101fad8bdd")
