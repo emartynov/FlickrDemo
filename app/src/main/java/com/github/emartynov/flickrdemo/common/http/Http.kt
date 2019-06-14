09449d7b60cbd26051ef5f94e5a0f0733f27ad97
Eugen Martynov <eugenemartynov@gmail.com>
2019-06-14 04:31:34 +0200

    Add debug logging

--- app/src/main/java/com/github/emartynov/flickrdemo/common/http/Http.kt
+++ app/src/main/java/com/github/emartynov/flickrdemo/common/http/Http.kt
@@ -1,5 +1,7 @@
 package com.github.emartynov.flickrdemo.common.http
 
+import android.util.Log
+import com.github.emartynov.flickrdemo.BuildConfig
 import java.io.BufferedInputStream
 import java.net.HttpURLConnection
 import java.net.URL
@@ -12,6 +14,10 @@ internal class HttpImpl : Http {
     override fun get(url: String): ByteArray {
         if (!(url.startsWith("http://") || url.startsWith("https://")))
             throw IllegalArgumentException("Supposed to be used only with http protocol in $url")
+
+        if (BuildConfig.DEBUG) {
+            Log.d("HTTP", "Loading data from $url")
+        }
 
         @Suppress("CAST_NEVER_SUCCEEDS") // we are sure it will be http connection
         val urlConnection = URL(url).openConnection() as HttpURLConnection
